dd7ae7e96ebc0a5f0ef661b4cd4a360576ef3c5d
backgom1 <dmstjd0214@naver.com>
2023-01-23 21:28:38 +0900

    피파 올리기

---
+++ Fifahasu/src/main/java/com/GangJin/Fifahasu/FifaHasuApplication.java
@@ -0,0 +1,13 @@
+package com.GangJin.Fifahasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaHasuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaHasuApplication.class, args);
+	}
+
+}
