8f9dbbc0e47c7aeda713e30e54b4b32c3224a6d5
ENSUNG\dmstjd <dmstjd0214@naver.com>
2023-10-31 22:40:19 +0900

    test : merge test

--- Fifahasu/src/main/java/com/GangJin/Fifahasu/controller/main/FifaApiController.java
+++ Fifahasu/src/main/java/com/GangJin/Fifahasu/controller/main/FifaApiController.java
@@ -1,10 +1,7 @@
 package com.GangJin.Fifahasu.controller.main;
 
-import com.GangJin.Fifahasu.controller.main.MainSearchForm;
-import com.GangJin.Fifahasu.service.fifaInfo.FIFAUserInfo;
-import com.GangJin.Fifahasu.service.fifaInfo.fifaInfoVO;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
