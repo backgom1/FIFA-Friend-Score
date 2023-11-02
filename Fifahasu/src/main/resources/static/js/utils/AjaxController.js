99a20174bb0f8e2eb9a2717f3060340053165535
ENSUNG\dmstjd <dmstjd0214@naver.com>
2023-11-03 01:38:21 +0900

    FEAT : 간단한 call 구현

---
+++ Fifahasu/src/main/resources/static/js/utils/AjaxController.js
@@ -0,0 +1,18 @@
+var AjaxController = {
+
+    call: function (url,type,data) {
+        console.log(url);
+        $.ajax({
+            url : url,
+            dataType : "json",
+            type : type,
+            contentType : "application/json",
+        }).done(function (data) {
+            console.log(data);
+            $("#searchInfo").replaceWith(data);
+        });
+    },
+    jsonCall: function () {
+
+    }
+}
